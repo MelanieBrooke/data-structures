d606c217e9f1d38f860c51d64e658042d06fa2a0
Miriam Wipf <miriam.wipf1@gmail.com>
2020-10-27 20:14:38 -0700

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,4 +1,4 @@
-var Tree = function(value) {
+var Tree = function (value) {
   var newTree = {};
   newTree.value = value;
   _.extend(newTree, treeMethods);
@@ -11,7 +11,7 @@ var Tree = function(value) {
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function (value) {
   //sets value as the target of a node
   // create variable for new tree, then
   // call the Tree function with the value as this new tree's value
@@ -20,11 +20,30 @@ treeMethods.addChild = function(value) {
   //adds that node as a child of the tree
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function (target) {
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i ++) {
+      var result = this.children[i].contains(target);
+      if (result) {
+        return true;
+      }
+
+    }
+  }
+
+  //return true
+  //if children, loop through
+  //result =   this.children[i].contains(target)
+  //
+  return false;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ //.addChild : O(1), constant time
+ //.contains : O(n), linear time
  */
