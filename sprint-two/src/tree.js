6ecd5c13b3ba3770fdfaa1f1cbc857411f4d54f1
Miriam Wipf <miriam.wipf1@gmail.com>
2020-10-27 19:53:54 -0700

    When this code is committed it will successfully implement the .addChild method to treeMethods

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,6 +12,12 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //sets value as the target of a node
+  // create variable for new tree, then
+  // call the Tree function with the value as this new tree's value
+  // push this tree onto the original tree children array using the variable name
+  this.children.push(Tree(value));
+  //adds that node as a child of the tree
 };
 
 treeMethods.contains = function(target) {
