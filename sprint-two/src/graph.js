fabab79f09d97d377868d0cce10370023e14bcc5
Miriam Wipf <miriam.wipf1@gmail.com>
2020-10-27 20:41:27 -0700

    When this code is committed it will successfully execute the .addNode and .contains functions

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,14 +2,26 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  var newNode = {};
+  newNode.value = node;
+  newNode.edges = [];
+  this.nodes.push(newNode);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  //iterate through nodes array to see if nodes[i]=== node
+  for (var i = 0; i < this.nodes.length; i ++) {
+    if (this.nodes[i].value === node) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Removes a node from the graph.
@@ -31,6 +43,8 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
 };
+
+var newGraph = new Graph;
 
 /*
  * Complexity: What is the time complexity of the above functions?
