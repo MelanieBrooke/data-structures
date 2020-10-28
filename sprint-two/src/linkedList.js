cc227083dd3ba70f12e0a1e54b472336cc7322d7
Miriam Wipf <miriam.wipf1@gmail.com>
2020-10-27 18:32:47 -0700

    test

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,39 @@
 var LinkedList = function() {
   var list = {};
   list.head = null;
+  //first node
   list.tail = null;
+  //last node or rest of list
 
   list.addToTail = function(value) {
+    //need to call Node to create a new node
+    var newNode = new Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      list.tail.next = list.head;
+    }
+    list.tail = newNode;
+    //should designate a new tail when new nodes are added
+    // The pointer next is assigned head pointer so that both pointers now point at the same node.
+
   };
 
   list.removeHead = function() {
+    //need to create new Node for new Head?
+    var newNode = new Node.value;
+    var headValue = list.head;
+    delete list.head;
+    newNode.next = list.head;
+    return headValue;
+    //should remove the head from the list when removeHead is called
+    //should return the value of the former head when removeHead is called
   };
 
   list.contains = function(target) {
+    //recursion to search nodes
+    //should contain a value that was added
+    //should not contain a value that wasn't added
   };
 
   return list;
@@ -27,3 +51,7 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+ //each element points to the next
+ //each node contains data and a reference (link) to the next node
