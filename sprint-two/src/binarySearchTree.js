2f9406ef97b0d2492177303ddef7f3b45f6d3c15
Miriam Wipf <miriam.wipf1@gmail.com>
2020-10-29 21:47:20 -0700

    When this code is committed it will have stubs for BinarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,32 @@
 var BinarySearchTree = function(value) {
+  this.left = [];
+  this.right =
+};
+
+BinarySearchTree.prototype.left = function () {
+
+
+
+};
+
+BinarySearchTree.prototype.right = function () {
+
+};
+
+BinarySearchTree.prototype.insert = function (value) {
+};
+
+//contains returns boolean
+BinarySearchTree.prototype.contains = function (value) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (cb) {
+
 };
 
 
+var searchTree = new BinarySearchTree;
 /*
  * Complexity: What is the time complexity of the above functions?
  */
